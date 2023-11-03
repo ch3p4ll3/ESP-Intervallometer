fbb558bd5962d20ec6b4bcd64b536fe2d00096b5
Mattia Vidoni <vidonimattia@gmail.com>
2023-11-03 12:01:12 +0100

    Update serviceWorker.js

--- Web-App/serviceWorker.js
+++ Web-App/serviceWorker.js
@@ -1,18 +1,18 @@
 const staticDevCoffee = "ESP-Intervallometer"
 const assets = [
-    "/",
-    "/index.html",
-    "/assets/css/bootstrap.min.css",
-    "/assets/css/easings.css",
-    "/assets/css/theme.css",
-    "/assets/js/bootstrap.min.js",
-    "/assets/js/constants.js",
-    "/assets/js/jquery-3.3.1.slim.min.js",
-    "/assets/js/popper.min.js",
-    "/assets/js/script.js",
-    "/assets/js/theme.js",
-    "/assets/icon/icon.svg",
-    "/assets/icon/icon.png"
+    "ESP-Intervallometer//",
+    "ESP-Intervallometer//index.html",
+    "ESP-Intervallometer//assets/css/bootstrap.min.css",
+    "ESP-Intervallometer//assets/css/easings.css",
+    "ESP-Intervallometer//assets/css/theme.css",
+    "ESP-Intervallometer//assets/js/bootstrap.min.js",
+    "ESP-Intervallometer//assets/js/constants.js",
+    "ESP-Intervallometer//assets/js/jquery-3.3.1.slim.min.js",
+    "ESP-Intervallometer//assets/js/popper.min.js",
+    "ESP-Intervallometer//assets/js/script.js",
+    "ESP-Intervallometer//assets/js/theme.js",
+    "ESP-Intervallometer//assets/icon/icon.svg",
+    "ESP-Intervallometer//assets/icon/icon.png"
 ]
 
 self.addEventListener("install", installEvent => {
